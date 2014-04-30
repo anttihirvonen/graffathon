b397cb08a05343f052cd968c74bdedc2c463526b
Antti Hirvonen <antti.hirvonen@gmail.com>
2014-04-30 14:24:05 +0300

    Add ProxyFix (for running in production)

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from werkzeug.contrib.fixers import ProxyFix
 
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
 app.config.from_object('app.settings.common')
 app.config.from_envvar('GRAFFATHON_SETTINGS')
 
