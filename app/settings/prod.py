1e71e8d51b3046ba91c9deafa7fca081ade2a894
Antti Hirvonen <antti.hirvonen@gmail.com>
2014-05-04 15:33:27 +0300

    Database settings for production

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -7,3 +7,5 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 COLLECT_STATIC_ROOT = os.environ['COLLECT_STATIC_ROOT']
 COLLECT_STORAGE = 'flask.ext.collect.storage.file'
+
+SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
