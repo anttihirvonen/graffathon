054092fdc31ca66612c6162f3fe9ef01c3c65812
Antti Hirvonen <antti.hirvonen@gmail.com>
2014-05-23 09:21:38 +0300

    Mail settings for dev/prod envs

--- app/settings/dev.py
+++ app/settings/dev.py
@@ -8,9 +8,17 @@ COLLECT_STORAGE = 'flask.ext.collect.storage.file'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/graffathon.db'
 
-USERNAME="admin"
-PASSWORD="password"
+USERNAME = "admin"
+PASSWORD = "password"
 
-#email server
-TESTING = True
-ADMINS = ['test@localhost.com']
+# email server
+# TESTING = True
+# ADMINS = ['test@localhost.com']
+
+# address from which mails are sent
+MAIL_DEFAULT_SENDER = 'graffathon@localhost'
+
+# for testing run SMTP server with
+# python -m smtpd -n -c DebuggingServer localhost:1026
+# MAIL_DEBUG = True
+MAIL_PORT = 1026
--- app/settings/prod.py
+++ app/settings/prod.py
@@ -12,3 +12,8 @@ SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
 
 USERNAME = os.environ['USERNAME']
 PASSWORD = os.environ['PASSWORD']
+
+# Mail is sent from localhost,
+# so no extra configuration is needed
+MAIL_SERVER = 'localhost'
+MAIL_DEFAULT_SENDER = 'dot@kapsi.fi'
